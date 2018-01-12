2feb62fad7a52c9bf2dd17dcf040d325ca502d34
Per Erik Stendahl <pererik@berrs.net>
2018-01-12 21:31:57 +0100

    Day 1, b, v2

--- Aoc2015/Solutions/Day1.cs
+++ Aoc2015/Solutions/Day1.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace Aoc2015.Tests
+namespace Aoc2015
 {
     public class Day1a
     {
@@ -13,17 +13,17 @@ namespace Aoc2015.Tests
 
     public class Day1b
     {
+        static int ValueOf(char c)
+        {
+            if (c == '(') return 1;
+            if (c == ')') return -1;
+            return 0;
+        }
+
         static IEnumerable<int> GetFloors(string input)
         {
             int floor = 0;
-            foreach (char c in input)
-            {
-                if (c == '(')
-                    floor++;
-                else if (c == ')')
-                    floor--;
-                yield return floor;
-            }
+            return input.Select(ValueOf).Select(x => { floor += x; return floor; });
         }
 
         public static int Calculate(string input)
