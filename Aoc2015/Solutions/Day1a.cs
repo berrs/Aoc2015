7fada3b09b085ac2877315493cad51d2cd9b5a1b
Per Erik Stendahl <pererik@berrs.net>
2018-01-12 20:56:32 +0100

    Day 1, a

---
+++ Aoc2015/Solutions/Day1a.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Aoc2015.Tests
+{
+    public class Day1a
+    {
+        public static int Calculate(string input)
+        {
+            return input.Count(c => c == '(') - input.Count(c => c == ')');
+        }
+    }
+}
---
+++ Aoc2015/Tests/TestBase.cs
@@ -0,0 +1,11 @@
+﻿using Xunit.Abstractions;
+
+namespace Aoc2015.Tests
+{
+    public abstract class TestBase
+    {
+        readonly ITestOutputHelper output;
+        protected TestBase(ITestOutputHelper output) => this.output = output;
+        protected void WriteLine(string message) => output.WriteLine(message);
+    }
+}
