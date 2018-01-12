aa538232185edc9f0b5c05f81670ea7772b5cbb3
Per Erik Stendahl <pererik@berrs.net>
2018-01-12 21:17:54 +0100

    Day 1, b

--- Aoc2015/Solutions/Day1a.cs
+++ Aoc2015/Solutions/Day1a.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Aoc2015.Tests
 {
@@ -7,6 +8,27 @@ namespace Aoc2015.Tests
         public static int Calculate(string input)
         {
             return input.Count(c => c == '(') - input.Count(c => c == ')');
+        }
+    }
+
+    public class Day1b
+    {
+        static IEnumerable<int> GetFloors(string input)
+        {
+            int floor = 0;
+            foreach (char c in input)
+            {
+                if (c == '(')
+                    floor++;
+                else if (c == ')')
+                    floor--;
+                yield return floor;
+            }
+        }
+
+        public static int Calculate(string input)
+        {
+            return GetFloors(input).TakeWhile(x => x >= 0).Count() + 1;
         }
     }
 }
---
+++ Aoc2015/Tests/Day1bTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Aoc2015.Tests
+{
+    public class Day1bTests : TestBase
+    {
+        public Day1bTests(ITestOutputHelper output) : base(output) { }
+
+        [Theory]
+        [InlineData(")", 1)]
+        [InlineData("()())", 5)]
+        public void Spec(string input, int expected)
+        {
+            Assert.Equal(expected, Day1b.Calculate(input));
+        }
+
+
+        [Fact]
+        public void CalculateDay1b()
+        {
+            WriteLine(Day1b.Calculate(Day1aTests.InputA).ToString());
+        }
+    }
+}
