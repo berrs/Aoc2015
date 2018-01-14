28b21f69aedbb675daba0434d4947ea54920af68
Per Erik Stendahl <pererik@berrs.net>
2018-01-15 00:27:03 +0100

    added fluent assertions

---
+++ Aoc2015/Tests/FluentAssertions.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace Aoc2015.Tests
+{
+    static class FluentAssertions
+    {
+        public static void ShouldEqual<T>(this T actual, T expected) => Assert.Equal(expected, actual);
+    }
+}
