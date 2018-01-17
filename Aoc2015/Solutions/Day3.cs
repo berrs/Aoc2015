492ec8957eec91fe0ea59f8963626a0e86be9b5b
Per Erik Stendahl <pererik@berrs.net>
2018-01-17 21:57:50 +0100

    Day 3, a, v2

--- Aoc2015/Solutions/Day3.cs
+++ Aoc2015/Solutions/Day3.cs
@@ -1,28 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace Aoc2015
 {
     public static class Day3
     {
-        static int XDiff(char c) => (c == '>' ? 1 : (c == '<' ? -1 : 0));
-        static int YDiff(char c) => (c == '^' ? 1 : (c == 'v' ? -1 : 0));
+        struct Point
+        {
+            int X, Y;
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public static Point operator +(Point p, char c)
+            {
+                return new Point(p.X + XDiff(c), p.Y + YDiff(c));
+            }
+
+            public override string ToString()
+            {
+                return $"{X}.{Y}";
+            }
+
+            static int XDiff(char c) => Diff(c, '>', '<');
+            static int YDiff(char c) => Diff(c, '^', 'v');
+            static int Diff(char c, char pos, char minus) => (c == pos ? 1 : (c == minus ? -1 : 0));
+        }
 
         public static class A
         {
             public static int Calculate(string input)
             {
                 var counters = new Dictionary<string, int>();
-                int x = 0;
-                int y = 0;
-                string lastpos = "0.0";
+                var p = new Point(0, 0);
+                string lastpos = p.ToString();
                 counters[lastpos] = 1;
                 foreach (char c in input)
                 {
-                    x += XDiff(c);
-                    y += YDiff(c);
-                    string pos = $"{x}.{y}";
+                    p = p + c;
+                    string pos = p.ToString();
                     if (pos != lastpos)
                     {
                         counters[pos] = (counters.TryGetValue(pos, out int previous) ? previous : 0) + 1;
