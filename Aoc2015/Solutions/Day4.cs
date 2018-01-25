fe278f8b457c675824f1b3a65597b6a502cee936
Per Erik Stendahl <pererik@berrs.net>
2018-01-26 00:03:48 +0100

    Day 4

---
+++ Aoc2015/Solutions/Day4.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Aoc2015
+{
+    public static class Day4
+    {
+        public static class A
+        {
+            public static int Calculate(string input)
+            {
+                var md5 = MD5.Create();
+                int i = 1;
+                for (; ; i++)
+                {
+                    var data = Encoding.ASCII.GetBytes(input + i);
+                    byte[] hash = md5.ComputeHash(data);
+                    if (hash[0] == 0 && hash[1] == 0 && hash[2] < 0x10)
+                    {
+                        break;
+                    }
+                }
+                return i;
+            }
+        }
+
+        public static class B
+        {
+            public static long Calculate(string input)
+            {
+                var md5 = MD5.Create();
+                long i = 1;
+                for (; ; i++)
+                {
+                    var data = Encoding.ASCII.GetBytes(input + i);
+                    byte[] hash = md5.ComputeHash(data);
+                    if (hash[0] == 0 && hash[1] == 0 && hash[2] == 0)
+                    {
+                        break;
+                    }
+                }
+                return i;
+            }
+        }
+    }
+}
---
+++ Aoc2015/Tests/Day4Tests.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Aoc2015.Tests.Day4Tests
+{
+    public class Day4aTests : TestBase
+    {
+        public Day4aTests(ITestOutputHelper output) : base(output) { }
+
+        [Theory]
+        [InlineData("abcdef", 609043)]
+        [InlineData("pqrstuv", 1048970)]
+        public void Spec(string input, int expected)
+        {
+            var sw = Stopwatch.StartNew();
+            Day4.A.Calculate(input).ShouldEqual(expected);
+            WriteLine(sw.Elapsed);
+        }
+
+        [Fact]
+        public void Run()
+        {
+            var sw = Stopwatch.StartNew();
+            WriteLine(Day4.A.Calculate("bgvyzdsv"));
+            WriteLine(sw.Elapsed);
+        }
+    }
+
+    public class Day4bTests : TestBase
+    {
+        public Day4bTests(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void Run()
+        {
+            var sw = Stopwatch.StartNew();
+            WriteLine(Day4.B.Calculate("bgvyzdsv"));
+            WriteLine(sw.Elapsed);
+        }
+    }
+}
