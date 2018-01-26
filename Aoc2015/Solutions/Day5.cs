8e76575f8ed82c2078438101e7b83158fadeb70a
Per Erik Stendahl <pererik@berrs.net>
2018-01-26 23:01:38 +0100

    Day 5, a

---
+++ Aoc2015/Solutions/Day5.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Aoc2015
+{
+    public static class Day5
+    {
+        public static class A
+        {
+            public static bool Calculate(string input)
+            {
+                return input.Count(c => "aeiou".Contains(c)) >= 3 &&
+                       input.Any(c => input.Contains($"{c}{c}")) &&
+                       !input.Contains("ab") &&
+                       !input.Contains("cd") &&
+                       !input.Contains("pq") &&
+                       !input.Contains("xy");
+            }
+        }
+    }
+}
---
+++ Aoc2015/Tests/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2015.Tests
+{
+    static class Utilities
+    {
+        public static IEnumerable<string> Tokenize(this string input)
+        {
+            return (input ?? "")
+                .Split('\n')
+                .Select(s => s.Trim())
+                .Where(s => !String.IsNullOrEmpty(s));
+        }
+    }
+}
